ba59f8ab813169049d76acc2a9f48e67d6947257
DaiZrei <82783596+DaiZrei@users.noreply.github.com>
2021-08-12 07:17:45 +0300

    Update MaterialBar.kt

--- materialbar/src/main/java/com/vanpra/materialbar/ui/MaterialBar.kt
+++ materialbar/src/main/java/com/vanpra/materialbar/ui/MaterialBar.kt
@@ -16,7 +16,7 @@ import androidx.core.content.ContextCompat
 class MaterialBarError(message: String) : Exception(message)
 
 class MaterialBar @JvmOverloads constructor(
-    context: Context,
+    context: AppCompatResources,
     attrs: AttributeSet? = null,
     defStyleRes: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleRes) {
@@ -72,7 +72,7 @@ class MaterialBar @JvmOverloads constructor(
             .inflate(R.layout.material_bar_layout, this, true)
 
         attrs?.let {
-            val typedArray = ContextCompat.obtainStyledAttributes(
+            val typedArray = context.obtainStyledAttributes(
                 it,
                 R.styleable.MaterialBar, 0, 0
             )
